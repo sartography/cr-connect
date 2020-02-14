5203dc0dc90b98a5dc62898bc130f24e578b3937
Fred McDavid <fred@rejoiner.com>
2020-02-13 20:54:17 -0500

    adds admin tasks to stop/start docker and rancher on rancher server, get the docker registry and certs working

--- tasks.py
+++ tasks.py
@@ -2,6 +2,8 @@ import os
 import subprocess
 from invoke import task, Collection
 
+SCRIPT_DIRPATH = os.path.dirname(os.path.realpath(__file__))
+
 
 def is_linux():
     uname_output = subprocess.Popen(["uname", "-a"],
@@ -103,6 +105,61 @@ def list_git_references(c):
     c.run('git branch -r')
 
 
+@task
+def stop_rancher__admin(c):
+    with c.cd(os.path.join(SCRIPT_DIRPATH, 'ansible')):
+        c.run(f'ansible-playbook -i inventories/staging --limit=admin --tags stop --skip-tags start stop-rancher.yaml')
+
+
+@task
+def start_rancher__admin(c):
+    with c.cd(os.path.join(SCRIPT_DIRPATH, 'ansible')):
+        c.run(f'ansible-playbook -i inventories/staging --limit=admin --tags start --skip-tags stop start-rancher.yaml')
+
+
+def run_ansible_staging(c, playbook, opts_str):
+    executable = 'ansible-playbook'
+    stdopt_inv = '-i inventories/staging'
+    cmd = f'{executable} {stdopt_inv} {opts_str} {playbook}'
+    c.run(cmd)
+
+
+@task
+def stop_docker_registry__admin(c):
+    with c.cd(os.path.join(SCRIPT_DIRPATH, 'ansible')):
+        run_ansible_staging(c, 'stop-docker-registry.yaml', '--limit=admin --tags stop --skip-tags start')
+
+
+@task
+def start_docker_registry__admin(c):
+    with c.cd(os.path.join(SCRIPT_DIRPATH, 'ansible')):
+        run_ansible_staging(c, 'start-docker-registry.yaml', '--limit=admin --tags start --skip-tags stop')
+
+
+@task(pre=[stop_rancher__admin], post=[start_rancher__admin])
+def request_new_docker_registry_cert__admin(c):
+    with c.cd(os.path.join(SCRIPT_DIRPATH, 'ansible')):
+        run_ansible_staging(c, 'update-docker-registry-cert.yaml', '--limit=admin')
+
+
+@task(pre=[stop_rancher__admin], post=[start_rancher__admin])
+def request_new_rancher_cert__admin(c):
+    with c.cd(os.path.join(SCRIPT_DIRPATH, 'ansible')):
+        run_ansible_staging(c, 'update-rancher-cert.yaml', '--limit=admin')
+
+@task
+def bash_shell__admin(c):
+    os.system('ssh ubuntu@34.237.245.238')
+
+
+@task
+def provision_servers__admin(c):
+    # provision admin server
+    with c.cd(os.path.join(SCRIPT_DIRPATH, 'ansible')):
+        for playbook in ['provision-docker-registry.yaml', 'provision-rancher.yaml']:
+            run_ansible_staging(c, playbook, '--limit=admin')
+
+
 # repo maintenance and info
 ns_repo = Collection('repo')
 ns_repo.add_task(clean)
@@ -117,6 +174,17 @@ ns_dev.add_task(stopenv, 'stop')
 ns_dev.add_task(restartenv, 'restart')
 ns_dev.add_task(buildenv, 'build')
 ns_dev.add_task(showenv, 'show')
+
+# staging constellation admin
+ns_admin = Collection('admin')
+ns_admin.add_task(stop_rancher__admin, 'stop-rancher')
+ns_admin.add_task(provision_servers__admin, 'provision')
+ns_admin.add_task(start_rancher__admin, 'start-rancher')
+ns_admin.add_task(stop_docker_registry__admin, 'stop-docker-registry')
+ns_admin.add_task(start_docker_registry__admin, 'start-docker-registry')
+ns_admin.add_task(request_new_docker_registry_cert__admin, 'req_new_docker_registry_cert')
+ns_admin.add_task(request_new_rancher_cert__admin, 'req_new_rancher_cert')
+ns_admin.add_task(bash_shell__admin, 'admin-srv-bash-shell')
 
 # docker maintenance
 ns_docker = Collection('docker')
@@ -134,9 +202,11 @@ ns_database = Collection('database')
 ns_database.add_task(dbshell, 'shell')
 ns_dev.add_collection(ns_database)
 
+
 # put dev in env namespace
 ns_env = Collection('env')
 ns_env.add_collection(ns_dev)
+ns_env.add_collection(ns_admin)
 
 # add namespaces to root collection
 ns = Collection()
